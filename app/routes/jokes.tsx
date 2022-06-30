937984e891fcd898c0441b2614163418f0e9576f
Marknjo <marknjo7@gmail.com>
2022-06-30 12:52:41 +0300

    Feat(routes): Add primary routes
    ./home - IndexRoute()
    ./jokes - layout (JokesRoute())
    ./jokes/home - JokesIndexRoute()
    ./jokes/new  - JokesNewRoute()
    ./jokes/:jokeId - find a single joke (JokeRoute)

---
+++ app/routes/jokes.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from '@remix-run/react';
+
+export default function JokesRoute() {
+  return (
+    <h1>
+      <h1>J😜KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </h1>
+  );
+}
