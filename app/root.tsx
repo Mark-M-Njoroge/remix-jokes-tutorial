a40fb119df28728a54fac5e10a6aa9eae55d918b
Marknjo <marknjo7@gmail.com>
2022-06-30 13:56:39 +0300

    Feat(Styles): add styles to jokes app

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import type { ReactNode } from 'react';
 import {
   Links,
@@ -8,12 +8,31 @@ import {
   //Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import globalStylesUrl from '~/styles/global.css';
+import globalMediumStylesUrl from '~/styles/global-medium.css';
+import globalLargeStylesUrl from '~/styles/global-large.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: "Remix: So great, it's funny!",
   viewport: 'width=device-width,initial-scale=1',
 });
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: globalStylesUrl },
+    {
+      rel: 'stylesheet',
+      href: globalMediumStylesUrl,
+      media: 'print, (min-width: 640px)',
+    },
+    {
+      rel: 'stylesheet',
+      href: globalLargeStylesUrl,
+      media: 'screen and (min-width: 1024px)',
+    },
+  ];
+};
 
 function Document({ children }: { children: ReactNode }) {
   return (
