3540072cbc2f788d4a73069a3ab85fe69a2585ef
Marknjo <marknjo7@gmail.com>
2022-07-02 15:09:58 +0300

    Feat(DB): Add users model initialized with Kody user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,21 @@
-import { db } from '~/utils/db.server';
-// import { PrismaClient } from '@prisma/client';
-// const db = new PrismaClient();
+// import { db } from '~/utils/db.server';
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
 
 async function seed() {
+  const kody = await db.user.create({
+    data: {
+      username: 'Kody Koala',
+      // "twixrox"
+      passwordHash:
+        '$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u',
+    },
+  });
+
   await Promise.all(
     getJokes().map(joke => {
-      return db.joke.create({ data: joke });
+      const data = { jokesterId: kody.id, ...joke };
+      return db.joke.create({ data });
     })
   );
 }
